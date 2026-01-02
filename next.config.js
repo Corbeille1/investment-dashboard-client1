6eda27a9e5c0586b81c66c46dba84d8539930568
Corbeille1 <amahali.we@gmail.com>
2026-01-02 21:17:41 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// This file is not needed for Vite setup - keeping for reference only
+// const createNextIntlPlugin = require('next-intl/plugin');
+// 
+// const withNextIntl = createNextIntlPlugin();
+// 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: {
+//       allowedOrigins: ['localhost:3000']
+//     }
+//   },
+//   images: {
+//     domains: ['lh3.googleusercontent.com'],
+//     formats: ['image/webp', 'image/avif']
+//   }
+// };
+// 
+// module.exports = withNextIntl(nextConfig);
